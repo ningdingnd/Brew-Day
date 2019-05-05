b7d23b5b5e848c47363db289283cbd89e994e918
helena615 <ning980615@gmail.com>
2019-05-05 18:46:42 +0800

    add test case in storage ingredient class

--- brewDay/src/brewDay/GUI.java
+++ brewDay/src/brewDay/GUI.java
@@ -2,7 +2,12 @@ package brewDay;
 
 public class GUI {
 	public static void main(String[] args) {
+		
+		/* ADD YOUR TEST HERE*/
+		StorageIngredientController.testStorageIngredientController();
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			
 			public void run() {
 				Workbench w = new Workbench();
 
--- brewDay/src/brewDay/StorageIngredientController.java
+++ brewDay/src/brewDay/StorageIngredientController.java
@@ -32,7 +32,52 @@ public class StorageIngredientController extends Controller{
 	        
 	      }
 	      statement.executeUpdate("UPDATE StorageIngredient SET amount = " + updateAmount 
-  	    		  + "WHERE ID = " + ID);
+  	    		  + " WHERE ID = " + ID);
+	      
+	      
+	    }
+	    catch(SQLException e)
+	    {
+	      // if the error message is "out of memory",
+	      // it probably means no database file is found
+	      System.err.println(e.getMessage());
+	    }
+	    finally
+	    {
+	      try
+	      {
+	        if(connection != null)
+	          connection.close();
+	      }
+	      catch(SQLException e)
+	      {
+	        // connection close failed.
+	        System.err.println(e.getMessage());
+	      }
+	    }
+	    return true;
+	}
+	public boolean subtractAmount(int ID, float amount) {
+	    Connection connection = null;
+	    try
+	    {
+	      // create a database connection
+	      connection = DriverManager.getConnection("jdbc:sqlite:data.db");
+	      Statement statement = connection.createStatement();
+	      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+	      float updateAmount = 0;
+	      //	get the correct record from table
+	      ResultSet rs = statement.executeQuery("select * from StorageIngredient");
+	      while(rs.next())
+	      {
+	        if(rs.getInt("ID") == ID) {
+	        	updateAmount = rs.getFloat("amount") - amount;
+	        }
+	        
+	      }
+	      statement.executeUpdate("UPDATE StorageIngredient SET amount = " + updateAmount 
+  	    		  + " WHERE ID = " + ID);
 	      
 	      
 	    }
@@ -58,10 +103,12 @@ public class StorageIngredientController extends Controller{
 	    return true;
 	}
 	
+	//	test function
 	public static void testStorageIngredientController() {
 		Workbench w = new Workbench();
 		StorageIngredientController s = new StorageIngredientController(w);
 		
 		s.addAmount(1, 5);
+		s.subtractAmount(1, 6);
 	}
 }
